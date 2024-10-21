fa8503a13281cccd74544bf5df744d9d10d9f779
Harsh Anand <harsh250600@gmail.com>
2024-10-21 16:02:15 +0530

    Footer component created with stylings

---
+++ demo-react/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './footer.module.css';
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>Copyright Harsh E-Com store &copy; 2024</footer>
+    </>
+  )
+}
