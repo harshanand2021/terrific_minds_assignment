ae7da01b9a439c5acf5bafe0f6c0b977605c810f
Harsh Anand <harsh250600@gmail.com>
2024-10-21 16:01:30 +0530

    Header component created with stylings

---
+++ demo-react/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>Harsh's E-Commerce Store</h1>
+      </header>
+    </>
+  );
+}
