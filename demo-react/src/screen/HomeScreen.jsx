58b934060da315e47e0a5cc7090bbc8bafacebe1
Harsh Anand <harsh250600@gmail.com>
2024-10-21 21:24:09 +0530

    HomeScreen component created

---
+++ demo-react/src/screen/HomeScreen.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <div className="container">
+        <div className="items">
+          <img src="images/img1.jpg" alt="item1" width="200px" height="200px" />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Leather Jacket Mens</span>
+            <br />
+            <h4>Price</h4> - <span>$500</span>
+          </div>
+        </div>
+        <div className="items">
+          <img src="images/img2.jpg" alt="item2" width="200px" height="200px" />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Mens Jeans</span>
+            <br />
+            <h4>Price</h4> - <span>$400</span>
+          </div>
+        </div>
+        <div className="items">
+          <img src="images/img3.jpg" alt="item3" width="200px" height="200px" />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Mens formal T-Shirt</span>
+            <br />
+            <h4>Price</h4> - <span>$400</span>
+          </div>
+        </div>
+        <div className="items">
+          <img src="images/img4.jpg" alt="item4" width="200px" height="200px" />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>US Polo Mens TShirt</span>
+            <br />
+            <h4>Price</h4> - <span>$460</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://assets.ajio.com/medias/sys_master/root/20230505/PaBY/6454a303d55b7d0c6396c03f/-473Wx593H-469447333-black-MODEL.jpg"
+            alt="img5"
+            height="200px"
+            width="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Puma Mens T-Shirt</span>
+            <br />
+            <h4>Price</h4> - <span>$370</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://rukminim2.flixcart.com/image/850/1000/xif0q/jacket/v/s/f/xl-no-pkr16-red-pkr-sports-original-imaghumknsp54xzb.jpeg?q=90&crop=false"
+            alt="item6"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Mens Hoodie</span>
+            <br />
+            <h4>Price</h4> - <span>$470</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://rukminim2.flixcart.com/image/850/1000/xif0q/shirt/t/s/t/xl-dss-plan-3-prexacreation-original-imagmamnupk4hyhh.jpeg?q=20&crop=false"
+            alt="item7"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Mens Formal Shirt</span>
+            <br />
+            <h4>Price</h4> - <span>$340</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://www.tistabene.com/cdn/shop/files/KRT-0287A.jpg?v=1702894752"
+            alt="item8"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Mens Kurta</span>
+            <br />
+            <h4>Price</h4> - <span>$550</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://mahezon.in/cdn/shop/files/IMG-20240304_132754_800x1026_crop_center@2x.jpg?v=1709893303"
+            alt="item9"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Womens Silk Saree</span>
+            <br />
+            <h4>Price</h4> - <span>$350</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://media.nihalfashions.com/media/catalog/product/cache/caa15edf98145413286703527de7b8dd/p/e/peach-net-womens-salwar-suit-nws-6703.jpg"
+            alt="item10"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Womens Salwar</span>
+            <br />
+            <h4>Price</h4> - <span>$340</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://assets.ajio.com/medias/sys_master/root/20231118/NKtn/6558bc96afa4cf41f5913a35/-473Wx593H-466789362-black-MODEL.jpg"
+            alt="item11"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Womens Sport Tshirt</span>
+            <br />
+            <h4>Price</h4> - <span>$250</span>
+          </div>
+        </div>
+        <div className="items">
+          <img
+            src="https://rukminim2.flixcart.com/image/850/1000/kvr01ow0/jean/v/l/u/30-womenjeans-33-perfect-fashion-original-imag8h352hkeuzpk.jpeg?q=90&crop=false"
+            alt="item12"
+            width="200px"
+            height="200px"
+          />
+          <div className="item-details">
+            <h4>Product Name - </h4> <span>Womens Jeans</span>
+            <br />
+            <h4>Price</h4> - <span>$150</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
